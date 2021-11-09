3123191ce58955d562723bc5fc6b013235cf8cc7
David Gruseck <david.gruseck@actinate.com>
2021-11-09 09:16:17 +0000

    remove tsx

--- src/index.js
+++ src/index.js
@@ -2,13 +2,7 @@ import React from "react";
 import { Image } from "react-native";
 import * as flag from "./flags/flagsIndex";
 
-interface Props {
-  isoCode: string;
-  size: number;
-  style?: any;
-}
-
-const CountryFlag = ({ isoCode, size, style }: Props) => {
+const CountryFlag = ({ isoCode, size, style }) => {
   // This switch case is just there because you can't name variables "in" and "do"
   switch (isoCode.toLowerCase()) {
     case "in":
@@ -19,18 +13,18 @@ const CountryFlag = ({ isoCode, size, style }: Props) => {
         />
       );
       break;
-    case "do":
-      return (
-        <Image
-          source={flag["dom"]}
-          style={[{ width: size * 1.6, height: size }, style]}
-        />
-      );
-      break;
+    // case "do":
+    //   return (
+    //     <Image
+    //       source={flag["dom"]}
+    //       style={[{ width: size * 1.6, height: size }, style]}
+    //     />
+    //   );
+    //   break;
     default:
       return (
         <Image
-          source={(flag as any)[isoCode.toLowerCase()]}
+          source={flag[isoCode.toLowerCase()]}
           style={[{ width: size * 1.6, height: size }, style]}
         />
       );
